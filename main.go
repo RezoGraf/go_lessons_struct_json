ce2cb165b44c2d1b736b68338c1a7b3f1449cf01
RezoGraf <RezoGraf@users.noreply.github.com>
2022-06-02 19:02:18 +0700

    Update main.go

--- main.go
+++ main.go
@@ -53,78 +53,44 @@ func jsonRequestFunc(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+	titleNoNil := jsonRequest.Title != nil
+	tagsLen4More := len(jsonRequest.Tags) >= 4
+	titleLenLess50 := len([]rune(*jsonRequest.Title)) < 50
 
 	switch {
-	case jsonRequest.Title == nil && len(jsonRequest.Tags) >= 4:
-		jsonError := ResponseJson{
-			Code:    401,
-			Message: "JSON must have Title",
-		}
-		c.JSON(401, jsonError)
-		return
-	case jsonRequest.Title != nil && len(jsonRequest.Tags) < 4:
-		jsonError := ResponseJson{
-			Code:    402,
-			Message: "JSON must have 4 or more Tags",
-		}
-		c.JSON(402, jsonError)
-		return
-	case jsonRequest.Title == nil && len(jsonRequest.Tags) < 4:
-		jsonError := ResponseJson{
-			Code:    403,
-			Message: "JSON must have Title and 4 or more Tags",
-		}
-		c.JSON(403, jsonError)
-		return
-	case len(*jsonRequest.Title) > 50 && len(jsonRequest.Tags) >= 4:
-		jsonError := ResponseJson{
-			Code:    404,
-			Message: "JSON must have Title 50 or less symbols",
-		}
-		c.JSON(404, jsonError)
-		return
-	case len(jsonRequest.Tags) < 4 && jsonRequest.Title != nil && len(*jsonRequest.Title) < 50:
-		jsonError := ResponseJson{
-			Code:    405,
-			Message: "JSON  must have 4 or more Tags",
-		}
-		c.JSON(405, jsonError)
-		return
-	case len(jsonRequest.Tags) < 4 && len(*jsonRequest.Title) > 50:
-		jsonError := ResponseJson{
-			Code:    406,
-			Message: "JSON must have Title 50 or less symbols and 4 or more Tags",
-		}
-		c.JSON(406, jsonError)
-		return
+	case !titleNoNil && tagsLen4More:
+		apiErrorResponse(401, "JSON must have Title", c)
+	case titleNoNil && !tagsLen4More:
+		apiErrorResponse(402, "JSON must have 4 or more Tags", c)
+	case !titleNoNil && !tagsLen4More:
+		apiErrorResponse(403, "JSON must have Title and 4 or more Tags", c)
+	case !titleLenLess50 && tagsLen4More:
+		apiErrorResponse(404, "JSON must have Title 50 or less symbols", c)
+	case !tagsLen4More && titleNoNil && titleLenLess50:
+		apiErrorResponse(405, "JSON must have 4 or more Tags", c)
+	case !tagsLen4More && titleLenLess50:
+		apiErrorResponse(406, "JSON must have Title 50 or less symbols and 4 or more Tags", c)
 	default:
-		var RequestSave []RequestJson
-		RequestSave = append(RequestSave, jsonRequest)
-		byteBuffer, err := ioutil.ReadFile("db.json")
-		if err != nil {
-			log.Println(err)
-			toFileSave, err := json.MarshalIndent(RequestSave, "", " ")
-			if err != nil {
-				log.Println(err)
-			}
-			err = ioutil.WriteFile("db.json", toFileSave, 0644)
-			if err != nil {
-				log.Println(err)
-			}
-			jsonOK := ResponseJson{
-				Code:    200,
-				Message: "File created, JSON saved",
-			}
-			c.JSON(http.StatusOK, jsonOK)
-			return
-		}
-		var byteRead []RequestJson
-		err = json.Unmarshal(byteBuffer, &byteRead)
-		if err != nil {
-			log.Println(err)
-		}
-		byteRead = append(byteRead, jsonRequest)
-		toFileSave, err := json.MarshalIndent(byteRead, "", " ")
+		apiOkResponse(&jsonRequest, c)
+	}
+}
+
+func apiErrorResponse(code int, message string, c *gin.Context) {
+	jsonError := ResponseJson{
+		Code:    code,
+		Message: message,
+	}
+	c.JSON(code, jsonError)
+	return
+}
+
+func apiOkResponse(jsonRequest *RequestJson, cont *gin.Context) {
+	var RequestSave []RequestJson
+	RequestSave = append(RequestSave, *jsonRequest)
+	byteBuffer, err := ioutil.ReadFile("db.json")
+	if err != nil {
+		log.Println(err)
+		toFileSave, err := json.MarshalIndent(RequestSave, "", " ")
 		if err != nil {
 			log.Println(err)
 		}
@@ -132,10 +98,26 @@ func jsonRequestFunc(c *gin.Context) {
 		if err != nil {
 			log.Println(err)
 		}
-		jsonOK := ResponseJson{
-			Code:    201,
-			Message: "JSON appended to file",
-		}
-		c.JSON(http.StatusOK, jsonOK)
+		apiErrorResponse(200, "File created, JSON saved", cont)
+		return
 	}
+	var byteRead []RequestJson
+	err = json.Unmarshal(byteBuffer, &byteRead)
+	if err != nil {
+		log.Println(err)
+	}
+	byteRead = append(byteRead, *jsonRequest)
+	toFileSave, err := json.MarshalIndent(byteRead, "", " ")
+	if err != nil {
+		log.Println(err)
+	}
+	err = ioutil.WriteFile("db.json", toFileSave, 0644)
+	if err != nil {
+		log.Println(err)
+	}
+	jsonError := ResponseJson{
+		Code:    202,
+		Message: "JSON appended to file",
+	}
+	cont.JSON(202, jsonError)
 }
